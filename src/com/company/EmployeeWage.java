26fe69f9e34fc7fe41b4314029fdd4bd2f2887d5
Om Chikane <chikaneom007@gmail.com>
2022-05-02 02:05:47 +0530

    added: To Calculate Dxaily wage

--- src/com/company/EmployeeWage.java
+++ src/com/company/EmployeeWage.java
@@ -3,5 +3,12 @@ package com.company;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+
+
+        int WAGE_PER_HOUR = 20;
+        int FULL_DAY_HOUR = 8;
+
+        int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        System.out.println("Daily Wage is "+ dailyWage);
     }
 }
