27c961895e07325eab7bc2dbc5668e3b0b45c804
Om Chikane <chikaneom007@gmail.com>
2022-05-02 01:49:47 +0530

    To Check Employee Present or not

--- src/com/company/EmployeeWage.java
+++ src/com/company/EmployeeWage.java
@@ -3,5 +3,13 @@ package com.company;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+
+        int IS_PRESENT = 1;
+        double empCheck = Math.floor(Math.random() *10)  % 2;
+        if(empCheck == 1){
+            System.out.println("present");
+        }else{
+            System.out.println("Absent");
+        }
     }
 }
