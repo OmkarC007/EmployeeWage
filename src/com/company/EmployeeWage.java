b5fedb1987606b2e5e4efd9e8f241ff506107cb4
Om Chikane <chikaneom007@gmail.com>
2022-05-02 02:12:32 +0530

    Added: To Calculate part time daily wage

--- src/com/company/EmployeeWage.java
+++ src/com/company/EmployeeWage.java
@@ -3,5 +3,15 @@ package com.company;
 public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
+
+
+        int WAGE_PER_HOUR = 20;
+        //int FULL_DAY_HOUR = 8;
+        int PART_TIME_HOUR = 4;
+
+        int dailyWage = WAGE_PER_HOUR * PART_TIME_HOUR;
+        System.out.println(" Part time Daily Wage is "+ dailyWage);
+
+
     }
 }
